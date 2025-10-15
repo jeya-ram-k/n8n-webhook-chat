2f65c3e76d1523cd8fd7d678da75ef188b427f90
jeya-ram-k <jeyaram.k.mca.2023@snsct.org>
2025-10-15 17:59:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
         allowedHosts: [
-          'n8n-webhook-chat.netlify.com',
+          'n8n-webhook-chat.netlify.app',
         ],
       },
       plugins: [react()],
